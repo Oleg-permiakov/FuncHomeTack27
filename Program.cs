c27d6d685ee07a8273752b294fc6bab8949e9491
Oleg <schika145@gmail.com>
2023-10-27 13:44:21 +0500

    Конец

--- Program.cs
+++ Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Принимает на вход число и выдаёт сумму цифр в числе.
+Console.WriteLine("Введите число");
+int namber = Convert.ToInt32(Console.ReadLine());
+int res = 0;
+int res1 = 0;
+
+int CreateSum(int namber) {
+
+while (namber > 10)
+{
+    res1 = namber % 10;
+    res = res + res1;
+    namber = namber / 10;
+}
+res = res + namber;
+return res;
+
+}
+int result = CreateSum(namber);
+Console.WriteLine("Сумма цифр будет - " + result);
